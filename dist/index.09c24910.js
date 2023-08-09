b724057ca2b1d4dc4d0193e33c50cf15b05f235a
Keil-ENZO <enzo.keil06@icloud.com>
2023-08-09 10:07:38 +0200

    Terminer: menu burger

--- dist/index.09c24910.js
+++ dist/index.09c24910.js
@@ -3,5 +3,10 @@ window.addEventListener("mousemove", (e)=>{
     mouse.style.top = e.y + "px";
     mouse.style.left = e.x + "px";
 });
+// Function pour le menu burger
+function Menu(e) {
+    let menu = document.querySelector("ul");
+    e.classList.contains("fa-bars") ? (e.classList.replace("fa-bars", "fa-xmark"), menu.classList.add("top-[80px]"), menu.classList.add("opacity-100")) : (e.classList.replace("fa-xmark", "fa-bars"), menu.classList.remove("top-[80px]"), menu.classList.remove("opacity-100"));
+}
 
 //# sourceMappingURL=index.09c24910.js.map
--- src/js/script.js
+++ src/js/script.js
@@ -6,5 +6,17 @@ window.addEventListener("mousemove", (e) => {
 });
 
 
+// Function pour le menu burger
+function Menu(e) {
+  let menu = document.querySelector("ul");
+  e.classList.contains("fa-bars")
+    ? (e.classList.replace("fa-bars", "fa-xmark"),
+      menu.classList.add("top-[80px]"),
+      menu.classList.add("opacity-100"))
+    : (e.classList.replace("fa-xmark", "fa-bars"),
+      menu.classList.remove("top-[80px]"),
+      menu.classList.remove("opacity-100"));
+}
+
 
 
